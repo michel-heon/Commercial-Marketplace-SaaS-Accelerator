c9e5d9eb4a6eb77c7bd3877f7a2c63e49d94b0ea
michel-heon <heon@cotechnoe.com>
2025-10-27 06:37:32 -0400

    Fix: Use userPrincipalName instead of displayName for SQL Server admin to handle special characters

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -512,8 +512,8 @@ az network vnet subnet create --resource-group $ResourceGroupForDeployment --vne
 
 Write-host "      ➡️ Create Sql Server"
 $userId = az ad signed-in-user show --query id -o tsv 
-$userdisplayname = az ad signed-in-user show --query displayName -o tsv 
-az sql server create --name $SQLServerName --resource-group $ResourceGroupForDeployment --location $Location  --enable-ad-only-auth --external-admin-principal-type User --external-admin-name $userdisplayname --external-admin-sid $userId --output $azCliOutput
+$userPrincipalName = az ad signed-in-user show --query userPrincipalName -o tsv 
+az sql server create --name $SQLServerName --resource-group $ResourceGroupForDeployment --location $Location  --enable-ad-only-auth --external-admin-principal-type User --external-admin-name $userPrincipalName --external-admin-sid $userId --output $azCliOutput
 Write-host "      ➡️ Set minimalTlsVersion to 1.2"
 az sql server update --name $SQLServerName --resource-group $ResourceGroupForDeployment --set minimalTlsVersion="1.2"
 Write-host "      ➡️ Add SQL Server Firewall rules"
